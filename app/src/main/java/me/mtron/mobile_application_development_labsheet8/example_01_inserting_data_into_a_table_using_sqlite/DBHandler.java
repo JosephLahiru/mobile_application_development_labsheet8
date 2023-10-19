d522c153f0865718c18df879b4ad9292a85dd525
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-19 12:03:03 +0530

    Created DBHandler.java for example_01_inserting_data_into_a_table_using_sqlite

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet8/example_01_inserting_data_into_a_table_using_sqlite/DBHandler.java
@@ -0,0 +1,39 @@
+package me.mtron.mobile_application_development_labsheet8.example_01_inserting_data_into_a_table_using_sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    private static final String databaseName = "employee_details";
+    private static final int ver = 1;
+
+    public DBHandler(Context context) {
+        super(context, databaseName, null, ver);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE employee(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, dep TEXT, salary TEXT);";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS employee_details";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addEmployee(Employee employee){
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", employee.getName());
+        contentValues.put("dep", employee.getDep());
+        contentValues.put("salary", employee.getSalary());
+        sqLiteDatabase.insert("employee", null, contentValues);
+        sqLiteDatabase.close();
+    }
+}
